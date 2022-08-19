00954e0721197f2b45bfb96892a0ea453f493314
George Carr-Smith <gca73@uclive.ac.nz>
2022-08-19 15:43:08 +1200

    Created the packing list #feature

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.project1
 
 import android.os.Bundle
+import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.project1.ui.theme.Project1Theme
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
@@ -18,11 +30,18 @@ import okhttp3.Request
 import java.io.IOException
 
 class MainActivity : ComponentActivity() {
+    private val items = listOf<String>(
+        "Skiis","Helmet", "Poles","Gloves","Lunch","Water Bottle","Coffee","Jacket","Socks","Boots","Shirt","Hoodie","Goggles"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            getMountainData("-43.4717","171.5264")?.let { Text(it) }
-            Text("World")
+           Project1Theme {
+                Surface {
+                    SkiList(packing = items)
+                }
+           }
         }
     }
 }
@@ -32,6 +51,8 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
@@ -40,6 +61,53 @@ fun DefaultPreview() {
     }
 }
 
+/**
+ * A list with each item in its own row with a checkbox
+ */
+@Composable
+fun SkiList(packing: List<String>) {
+
+//    //Make a header
+//    Text(
+//        text = "Ski List",
+//        modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp),
+//        style = MaterialTheme.typography.headlineMedium
+//    )
+    LazyColumn(modifier = Modifier.fillMaxSize()
+        .padding(start = 16.dp, top = 16.dp, end = 16.dp)) {
+        items(packing) { item ->
+            SkiItem(item)
+            //divider with hex color #d3d3d3
+            Divider(color = Color(0x11000100))
+        }
+    }
+    
+}
+
+
+/**
+ * An item with a checkbox and text in same row
+ */
+@Composable
+fun SkiItem(item: String) {
+    Row(
+
+    ) {
+        val checked = remember { mutableStateOf(false) }
+
+        Checkbox(
+            checked = checked.value,
+            onCheckedChange = { checked.value = it },
+            colors = CheckboxDefaults.colors(),
+            modifier = Modifier.wrapContentHeight().padding(vertical = 33.dp)
+        )
+        Text(text = item,
+        style = MaterialTheme.typography.headlineMedium, modifier = Modifier.wrapContentHeight().padding(vertical = 25.dp))
+    }
+}
+
+
+@Composable
 fun getMountainData(latitude:String, longitude:String): String? {
     val url =
         "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=$latitude&lon=$longitude";
@@ -53,15 +121,7 @@ fun getMountainData(latitude:String, longitude:String): String? {
         .addHeader("X-RapidAPI-Host", "weatherbit-v1-mashape.p.rapidapi.com")
         .build()
 
-    client.newCall(request).enqueue(object : okhttp3.Callback {
-        override fun onFailure(call: okhttp3.Call, e: IOException) {
-            e.printStackTrace()
-        }
-        override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
-            result = response.body?.string()
-
-
-        }
-    })
+    val response = client.newCall(request).execute()
+    result = response.body?.string()
     return result
 }
---
+++ app/src/main/java/com/example/project1/PackingList.kt
@@ -0,0 +1,10 @@
+package com.example.project1
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun PackingList(){
+    Text(text = "Hello World")
+}
